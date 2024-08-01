58704f6826c60bc6bffef45365b30f1426527537
la .panon. <panno.800k@gmail.com>
2024-08-01 15:53:37 +0900

    remove meanless

--- src/gdextwiz.nim
+++ src/gdextwiz.nim
@@ -1,25 +1,19 @@
 import std/os
 import std/tables
 
-import sdk
-import subcommands/nimble
 import subcommands/library
-
-builtin_apps.add "help"
+import subcommands/initialize
 
 when isMainModule:
   import cligen
-  import std/sequtils
 
   proc mergeParams(cmdNames: seq[string]; cmdLine = commandLineParams()): seq[string] = #{.raises: [], tags: [], forbids: [].} =
     result = cligen.mergeParams(cmdNames, cmdLine)
     if cmdNames == @["multi"]:
-      let builtin_apps = builtin_apps.mapIt(optionNormalize(it))
-      if result.len > 0 and optionNormalize(result[0]) notin builtin_apps:
-        let subcommand = cmdline[0]
-        let args = cmdline[1..^1]
-        if findExe("gdextwiz-" & subcommand) != "":
-          quit execShellCmd("gdextwiz-" & subcommand & " " & args.join(" "))
+      let subcommand = cmdline[0]
+      let args = cmdline[1..^1]
+      if findExe("gdextwiz-" & subcommand) != "":
+        quit execShellCmd("gdextwiz-" & subcommand & " " & args.join(" "))
 
   dispatchmulti(
     [ "multi",
@@ -41,7 +35,7 @@ Extention:
   For example, if "gdextwiz-launch" command is available on ~/.nimble/bin,
   you can execute it with type "gdextwiz plugin launch" or "gdextwiz launch"."""
     ],
-    [nimble.check],
     [library.install, help= library.HELP install],
     [library.uninstall, help= library.HELP uninstall],
+    [initialize.init],
   )
